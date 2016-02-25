00c7735d9f144d23eb4a47bf7311dd8b4aa06cc4
PavelNikitinskiy <paveln@elephantheadsoft.com>
2016-02-25 13:44:05 +0200

    Todo list

---
+++ client/js/app.js
@@ -0,0 +1,27 @@
+var app = angular.module('todoApp', ['lbServices']);
+app.controller('todoController', function($scope, $http, Todo){
+  $scope.todos = Todo.find();
+  $scope.todo;
+  $scope.loading = false;
+  $scope.add = function(){
+    $scope.loading=true;
+    Todo.create({title: $scope.todo.title, isDone:false}).$promise
+      .then(function(todo){
+        $scope.todos.push(todo);
+        $scope.todo.title='';
+        $scope.loading=false;
+      });
+  };
+  $scope.delete = function($index){
+    $scope.loading=true;
+    var todo = $scope.todos[$index];
+    Todo.deleteById({id: todo.id}).$promise
+      .then(function(){
+        $scope.todos.splice($index,1);
+        $scope.loading=false;
+      });
+  };
+  $scope.update = function(todo){
+    todo.$save();
+  };
+});
